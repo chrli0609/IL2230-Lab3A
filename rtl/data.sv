caf8697744f1ab48bff5065473f884962a5dfccf
Christopher Liu <christopher.liu.0609@gmail.com>
2023-12-05 14:58:22 +0100

    added weight.sv and updated the parameters for data.sv

--- rtl/data.sv
+++ rtl/data.sv
@@ -1,5 +1,10 @@
-module data #() (
-
+module data #(
+    //Decide how big the shift register needs to be
+    parameter N,
+    parameter M,
+    parameter WIDTH
+) (
+    //Need to have 2*N memory segments (one for each input and ouput in each neuron layer)
 
 );
 
---
+++ rtl/weight.sv
@@ -0,0 +1,9 @@
+module weight #(
+    parameter N,
+    parameter M,
+    parameter WIDTH
+)(
+    //Need to have space for N*N*(M-1) memory segments
+);
+
+endmodule
