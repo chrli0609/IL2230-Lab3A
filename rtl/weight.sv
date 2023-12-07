b8ad8120f3c4e9df390d1e6099ec53862dd9dda9
Christopher Liu <christopher.liu.0609@gmail.com>
2023-12-07 21:41:02 +0100

    weight.sv should be working now

--- rtl/weight.sv
+++ rtl/weight.sv
@@ -4,16 +4,10 @@ module weight #(
     parameter WIDTH
 )(
   input logic [$clog2(M)-1:0] layer_addr, //Need to have space for N*N*(M-1) words
-  output logic [WIDTH-1:0] weights_out [N-1:0][(1+N)-1:0] // weights and bias for 1 layer
+  output logic signed [WIDTH-1:0] weights_out [N-1:0][(1+N)-1:0] // weights and bias for 1 layer
 );
-  logic signed [WIDTH-1:0] weights [M-1:0] [N-1:0] [(1+N)-1:0] // layer, neuron, weights+bias
+  logic signed [WIDTH-1:0] weights_memory [M-1:0] [N-1:0] [(1+N)-1:0]; // layer, neuron, weights+bias
   
-  
-  
-  initial begin
-    weights = $random;
-  end  
-  
-  assign weights_out = weight [layer_addr];
+  assign weights_out = weights_memory[layer_addr];
   
 endmodule
