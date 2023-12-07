b70365c620b332004bbf6bc4642b0dd2f33a962b
Christopher Liu <christopher.liu.0609@gmail.com>
2023-12-07 21:38:46 +0100

    have now verified data.sv

--- rtl/data.sv
+++ rtl/data.sv
@@ -5,34 +5,41 @@ module data #(
     parameter WIDTH
 ) (
     //Need to have 2*N memory segments (one for each input and ouput in each neuron layer)
-    input logic clk, rst_n, read_write_enable,  //0 is read, 1 is write
+    input logic rst_n, write_enable,  //0 is read, 1 is write
     input logic shift,                          //For shifting all the data from memory_store_output to memory_for_input (used during the shift state)
     input logic [WIDTH-1:0] in_data,            //From one_neuron.results
-    input logic [N-1:0] access_address,         //To specify which address space we are reading from/writing to
+    input logic [$clog2(N):0] write_address,         //To specify which address space we are reading from/writing to
 
-    output logic [WIDTH-1:0] out_data           //The data that is sent into the neuron for calculation (to be assigned to one_neuron.x)
+    output logic [WIDTH-1:0] out_data [N-1:0]          //The data that is sent into the neuron for calculation (to be assigned to one_neuron.x)
 );
 
 //Two memories
 logic [WIDTH-1:0] memory_for_input [N-1:0];         //For storing the input data (will be read and assigned to one_neuron.x)
 logic [WIDTH-1:0] memory_store_output [N-1:0];      //For storing the output from the neuron (will be assigned from one_neuron.result)
 
-always_ff @(posedge clk) begin
-    //If shift then we move all data from memory_store_output to memory_for input
-    if (shift) begin
-        memory_for_input = memory_store_output;
-        memory_store_output = '{N{'0}};
-    end
-    else begin
-        //Read from memory
-        if (!read_write_enable) begin
-            out_data = memory_for_input[access_address];
+always_comb begin
+    //If we are NOT resetting
+    if (rst_n) begin
+        //If shift then we move all data from memory_store_output to memory_for input
+        //NOTE: if we shift then we can neither read nor write!!!
+        if (shift) begin
+            memory_for_input = memory_store_output;
+            memory_store_output = '{N{'0}};
+        end
+        else begin
+            //Read from memory
+                out_data = memory_for_input;
+
+            //Write to memory
+            if(write_enable) begin
+                memory_store_output[write_address] = in_data;
+            end
         end
 
-        //Write to memory
-        else begin
-            memory_store_output[access_address] = in_data;
-        end
+    //If we are resetting
+    end else begin
+        memory_for_input = '{N{'0}};
+        memory_store_output = '{N{'0}};
     end
 
 end
--- rtl/data_tb.sv
+++ rtl/data_tb.sv
@@ -4,27 +4,28 @@ module data_tb #(
     parameter WIDTH = 8)();
 
 //Input output of the data module
-logic clk, rst_n, read_write_enable, shift;
+logic clk, rst_n, write_enable, shift;
 logic [WIDTH-1:0] in_data;
-logic [N-1:0] access_address;
-logic [WIDTH-1:0] out_data;
+logic [$clog2(N):0] write_address;
+logic [WIDTH-1:0] out_data [N-1:0];
 
 
 //To help see internal signals on waveform
-logic [WIDTH-1:0] memory_for_input_dut [N-1:0] = dut.memory_for_input;
-logic [WIDTH-1:0] memory_store_output_dut [N-1:0] = dut.memory_store_output;
+logic [WIDTH-1:0] memory_for_input_tmp [N-1:0];
+logic [WIDTH-1:0] memory_store_output_tmp [N-1:0];
 
 
 //Variables for debugging
 int clk_counter;
+logic test_passed;
+
 
 data #(N, M, WIDTH) dut(
-    .clk(clk),
     .rst_n(rst_n),
-    .read_write_enable(read_write_enable),
+    .write_enable(write_enable),
     .shift(shift),
     .in_data(in_data),
-    .access_address(access_address),
+    .write_address(write_address),
     .out_data(out_data)
 );
 
@@ -53,21 +54,75 @@ initial begin
 end
 
 always @(posedge clk) begin
-    read_write_enable = $random;
+
+    memory_for_input_tmp = dut.memory_for_input;
+    memory_store_output_tmp = dut.memory_store_output;
+
+    #1;
+
+    write_enable = $random;
     in_data = $random;
-    access_address = $urandom_range(0, N-1);
+    write_address = $urandom_range(0, N-1);
     shift = 0;
 
     if (clk_counter % 10 == 0) shift = 1;
 
-    #2;
+    #1;
 
+    test_passed = 0;
+
+    //Make assertions
+    if (!shift) begin
+        if (write_enable) begin
+            if (in_data == dut.memory_store_output[write_address]) test_passed = 1;
+        end
+
+        if (out_data == dut.memory_for_input) test_passed = 1;
+    end else begin
+        if (dut.memory_for_input == memory_store_output_tmp && dut.memory_store_output == '{N{'0}}) test_passed = 1;
+    end
+
+    if (test_passed) $display("TEST PASSED!");
+    else $display("TEST FAILED\n",
+        "rst_n: %b \n", rst_n,
+        "write_enable: %b \n", write_enable,
+        "in_data: %f \n", in_data,
+        "write_address: %d \n", write_address,
+        "shift: %b \n",
+        "memory_for_input[0]: %f \n", dut.memory_for_input[0],
+        "memory_for_input[1]: %f \n", dut.memory_for_input[1],
+        "memory_for_input[2]: %f \n", dut.memory_for_input[2],
+        "memory_for_input[3]: %f \n", dut.memory_for_input[3],
+        "memory_for_input[4]: %f \n", dut.memory_for_input[4],
+        "memory_store_output[0]: %f \n", dut.memory_store_output[0],
+        "memory_store_output[1]: %f \n", dut.memory_store_output[1],
+        "memory_store_output[2]: %f \n", dut.memory_store_output[2],
+        "memory_store_output[3]: %f \n", dut.memory_store_output[3],
+        "memory_store_output[4]: %f \n", dut.memory_store_output[4],
+        "out_data[0]: %f \n", out_data[0],
+        "out_data[1]: %f \n", out_data[1],
+        "out_data[2]: %f \n", out_data[2],
+        "out_data[3]: %f \n", out_data[3],
+        "out_data[4]: %f \n", out_data[4]
+        );
+
+        #1;
+    
+end
+
+
+endmodule
+
+
+
+
+/*
     $display("____________________________________________");
     $display("clk_counter: %d", clk_counter);
     $display("rst_n: %b", rst_n);
-    $display("read_write_enable: %b", read_write_enable);
+    $display("write_enable: %b", write_enable);
     $display("in_data: %f", in_data);
-    $display("access_address: %d", access_address);
+    $display("write_address: %d", write_address);
     $display("shift: %b", shift);
 
 
@@ -91,9 +146,9 @@ always @(posedge clk) begin
     //end
 
 
-    $display("out_data: %f", out_data);
-end
-
-
-
-endmodule
+    $display("out_data[0]: %f", out_data[0]);
+    $display("out_data[1]: %f", out_data[1]);
+    $display("out_data[2]: %f", out_data[2]);
+    $display("out_data[3]: %f", out_data[3]);
+    $display("out_data[4]: %f", out_data[4]);
+    */
